0731d42a1a438cc57316648a1597c3daac6c7278
uloydev <uloydev@gmail.com>
2021-01-23 16:54:56 +0700

    [ADD] config class documentation

--- config.py
+++ config.py
@@ -1,12 +1,28 @@
-import pymysql.cursors
+from pymysql.cursors import DictCursor
+
 
 class Config():
+    host: str
+    user: str
+    password: str
+    db: str
+    cursorclass: DictCursor
+
     def __init__(self):
+        """Fungsi Constructor dari class Config.
+        
+        digunakan untuk set akun database mysql
+        """
         self.host = 'localhost'
         self.user = 'root'
         self.password ='passwordnya'
         self.db = 'karchiz'
-        self.cursorclass = pymysql.cursors.DictCursor
+        self.cursorclass = DictCursor
 
-    def get(self):
+    def get(self) -> dict:
+        """Fungsi get config.
+
+        Returns:
+            dict: dictionary gabungan dari semua variabel / properti dari class config
+        """
         return self.__dict__
