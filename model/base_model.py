6ad2a180765e9b613092d18cbf5cf13b64c0b44e
uloydev <uloydev@gmail.com>
2021-01-23 21:01:52 +0700

    [ADD] base_model and server docs

--- model/base_model.py
+++ model/base_model.py
@@ -6,12 +6,24 @@ class BaseModel:
     _table: str
 
     def __init__(self):
+        """Fungsi constructor untuk class BaseModal dan akan mengisi properti _mysql dengan koneksi database."""
         self._mysql = DB.connection()
     
     def insert(self, data: dict) -> bool:
+        """Fungsi insert digunakan untuk menginput data kedalam suatu tabel.
+
+        Args:
+            data (dict): dictionary data berupa nama kolom (key) dan value 
+
+        Returns:
+            bool: true jika berhasil melakukan query ke database
+        """
+        # get columns names and values
         __columns = data.keys()
         __values = [ f'"{val}"' for val in data.values()]
+        # build query
         __query = f'INSERT INTO {self._table}({",".join(__columns)}) VALUES ({",".join(__values)})'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
@@ -22,7 +34,14 @@ class BaseModel:
             return False
 
     def get_all(self) -> list:
+        """Fungsi get_all digunakan  untuk mengambil semua data di dalam suatu tabel.
+
+        Returns:
+            list: list / array dari hasil query
+        """
+        # build query
         __query = f'SELECT * FROM {self._table}'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
@@ -33,7 +52,14 @@ class BaseModel:
             return []
 
     def get(self) -> dict:
+        """Fungsi get digunakan untuk mengambil satu baris data di suatu tabel.
+
+        Returns:
+            dict: dictionary dari kolom (key) dan value
+        """
+        # build query
         __query = f'SELECT * FROM {self._table}'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
@@ -44,7 +70,17 @@ class BaseModel:
             return {}
 
     def get_where(self, condition: str) -> list:
+        """Fungsi get_where digunakan untuk mengambil semua data di suatu tabel dengan kondisi tertentu.
+
+        Args:
+            condition (str): where statement
+
+        Returns:
+            list: list / array dari hasil query
+        """
+        # build query
         __query = f'SELECT * FROM {self._table} WHERE {condition}'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
@@ -55,8 +91,20 @@ class BaseModel:
             return []
 
     def update(self, data: dict, _id: int) -> bool:
+        """Fungsi update digunakan untuk memperbarui suatu data yang ada di suatu tabel.
+
+        Args:
+            data (dict): dictionary data berupa nama kolom (key) dan value
+            _id (int): id dari data yang akan diupdate
+
+        Returns:
+            bool: true jika berhasil melakukan query ke database
+        """
+        # menyiapkan array dari value baru
         __items = [ f'{key}="{data[key]}"' for key in data]
+        # build query
         __query = f'UPDATE {self._table} SET {",".join(__items)} WHERE id = {_id}'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
@@ -67,7 +115,17 @@ class BaseModel:
             return False
 
     def delete(self, _id: int) -> bool:
+        """Fungsi delete digunakan untuk menghapus suatu data yang ada di suatu tabel.
+
+        Args:
+            _id (int): id dari data yang ingin dihapus
+
+        Returns:
+            bool: true jika berhasil melakukan query ke database
+        """
+        # build query
         __query = f'DELETE FROM {self._table} WHERE id = {_id}'
+        # try to send query to database
         try:
             __cursor = self._mysql.cursor()
             __cursor.execute(__query)
--- server.py
+++ server.py
@@ -5,8 +5,13 @@ from controller import *
 class Server:
     @staticmethod
     def run():
+        """Fungsi static run digunakan untuk menjalankan server web."""
+        # inisiasi Flask framework app
         app = Flask("__main__", static_url_path="/assets", template_folder="frontend/html", static_folder="frontend/assets")
+        # set secret key
         app.secret_key = "secret_banget_ini_bro"
+        # register controllers
         CustomerController.register(app, route_base='/')
         AuthController.register(app, route_base='/')
+        # run app
         app.run(debug=True)
