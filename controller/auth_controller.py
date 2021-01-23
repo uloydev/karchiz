5d7794c4d307818823372ebccdd50bb53f4dc4e7
uloydev <uloydev@gmail.com>
2021-01-23 22:11:47 +0700

    [ADD] docs for controllers and ticket model

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -5,35 +5,59 @@ from model import UserModel
 
 class AuthController(FlaskView):
     @route('/login', methods=['GET', 'POST'])
-    def login_page(self):
+    def login_page(self) -> str:
+        """Fungsi login_page digunakan untuk menampilkan halaman login.
+
+        Returns:
+            str: html text
+        """
+        # cek jika user sudah login maka redirect ke home page
         if session.get('username'):
             flash("Kamu sudah login", 'error')
             return redirect(url_for("CustomerController:home_0"))
+        # cek jika request POST
         if request.method == 'POST':
             user_model = UserModel()
+            # cek jika berhasil login maka redirect ke home page
             if user_model.check_login(**request.form):
                 session['username'] = request.form.get('username')
                 return redirect(url_for("CustomerController:home_0"))
+            # jika gagal login maka kembali ke halaman login
             else:
                 flash('Username Dan Password Salah', 'error')
         return render_template("login.html")
 
     @route('/register', methods=['GET', 'POST'])
     def register_page(self):
+        """Fungsi register_page digunakan untuk menampilkan halaman register.
+
+        Returns:
+            str: html text
+        """
+        # cek jika user sudah login maka redirect ke home page
         if session.get('username'):
             flash("Kamu sudah login", 'error')
             return redirect(url_for("CustomerController:home_0"))
+        # cek jika request POST
         if request.method == 'POST':
             user_model = UserModel()
+            # cek jika berhasil input user maka redirect ke login page
             if user_model.insert(request.form):
                 flash("registrasi sukses, silahkan Login!", 'success')
                 return redirect(url_for("AuthController:login_page"))
+            # jika gagal login maka kembali ke halaman register
             else:
                 flash('Registrasi Gagal!', 'error')
         return render_template("register.html")
 
     @route('/logout')
-    def logout(self):
+    def logout(self) -> str:
+        """Fungsi logout digunakan untuk keluar dari akun.
+
+        Returns:
+            str: html text
+        """
+        # membersihkan sesi user dan kembali ke halaman login
         session.clear()
         flash('Sukses logout akun', 'success')
         return redirect(url_for('AuthController:login_page'))
--- controller/customer_controller.py
+++ controller/customer_controller.py
@@ -10,73 +10,128 @@ class CustomerController(FlaskView):
 
     @route('/')
     @route('/home')
-    def home(self):
+    def home(self) -> str:
+        """Fungsi home digunakan untuk menampilkan halaman home.
+
+        Returns:
+            str: html text
+        """
+        # inisiasi model yang diperlukan
         event_model = EventModel()
         ticket_model = TicketModel()
         order_model = OrderModel()
+
+        # cek jika user belum login maka redirect ke halaman login
         if not session.get('username'):
             flash('Kamu Belum Login!', 'error')
             return redirect(url_for("AuthController:login_page"))
+
+        # mengambil semua data events dari database
         events = event_model.get_all()
-        events = event_model.parseList(events)[-3:]
+        # parsing events agar menjadi list dari object Event dan diambil tiga event terbaru
+        events = event_model.parseList(events)
+        # mengambil data ticket dari semua events
         events = ticket_model.get_event_tickets(events)
+
+        # parsing orders agar menjadi list dari object Order dan diambil dua order terbaru
         orders = order_model.get_user_orders()[-2:]
+        # mengambil data ticket dari semua orders
         orders = ticket_model.get_order_ticket(orders)
         return render_template("home.html", events = events, orders=orders)
 
     @route('/profile', methods=['GET', 'POST'])
-    def profile(self):
+    def profile(self) -> str:
+        """Fungsi profile digunakan untuk menampilkan halaman profile.
+
+        Returns:
+            str: html text
+        """
+        # inisiasi model yang diperlukan
+        user_model = UserModel()
+
+        # cek jika user belum login maka redirect ke halaman login
         if not session.get('username'):
             flash('Kamu Belum Login!', 'error')
             return redirect(url_for("AuthController:login_page"))
         
-        user_model = UserModel()
+        # cek jika request POST
         if request.method == 'POST':
+            # mengambil data dari input user menjadi dictionary
             form = request.form.to_dict()
+            # handle upload file avatar user
             avatar = request.files['avatar']
             if avatar:
                 filename = secure_filename(avatar.filename)
                 avatar.save(os.path.join('./frontend/assets/uploads', filename))
                 form['avatar'] = filename
+            
+            # cek jika update user berhasil
             if user_model.update(form, form['id']):
                 flash("Berhasil update data user!", 'success')
             else:
                 flash('Gagal update data user!', 'error')
         
+        # mengambil data user yang sedang login
         current_user = user_model.get_current_user()
-        print(current_user)
         return render_template("profile.html", current_user=current_user)
 
     @route('/orders')
     def orders(self):
+        """Fungsi orders digunakan untuk menampilkan halaman orders.
+
+        Returns:
+            str: html text
+        """
+        # inisiasi model yang diperlukan
+        order_model = OrderModel()
+        ticket_model = TicketModel()
+
+        # cek jika user belum login maka redirect ke halaman login
         if not session.get('username'):
             flash('Kamu Belum Login!', 'error')
             return redirect(url_for("AuthController:login_page"))
 
-        order_model = OrderModel()
-        ticket_model = TicketModel()
+        # mengambil semua data order yang dimiliki user yang sedang login
         orders = order_model.get_user_orders()
+        # mengambil data tiket dari order yang dimiliki user yang sedang login
         orders = ticket_model.get_order_ticket(orders)
         return render_template("orders.html", orders=orders)
 
     @route('/events')
     def events(self):
-        if not session.get('username'):
-            flash('Kamu Belum Login!', 'error')
-            return redirect(url_for("AuthController:login_page"))
+        """Fungsi events digunakan untuk menampilkan halaman events.
+
+        Returns:
+            str: html text
+        """
+        # inisiasi model yang diperlukan
         event_model = EventModel()
         category_model = CategoryModel()
         ticket_model = TicketModel()
+        
+        # cek jika user belum login maka redirect ke halaman login
+        if not session.get('username'):
+            flash('Kamu Belum Login!', 'error')
+            return redirect(url_for("AuthController:login_page"))
         current_category = request.args.get('category')
+
+        # cek jika kategori yng dipilih tidak kosong dan bukan 'all'
         if current_category and current_category != 'all':
+            # mengambil semua events sesuai kategori yang dipilih
             current_category = int(current_category)
             events = event_model.get_category(int(current_category))
         else:
+            # mengambil semua events dari database
             events = event_model.get_all()
             current_category = 'all'
+        
+        # parsing events agar menjadi list dari object Event
         events = event_model.parseList(events)
+        # mengambil data ticket dari semua events
         events = ticket_model.get_event_tickets(events)
+        # mengambil semua data category di database
         categories = category_model.get_all()
+        # parsing categories agar menjadi list dari object Category
         categories = category_model.parseList(categories)
         return render_template("events.html", 
             events=events,
@@ -85,15 +140,27 @@ class CustomerController(FlaskView):
 
     @route('/place-order', methods=['POST'])
     def place_order(self):
-        if not session.get('username'):
-            flash('Kamu Belum Login!', 'error')
-            return redirect(url_for("AuthController:login_page"))
+        """Fungsi place_order digunakan untuk input order ke database.
+
+        Returns:
+            str: html text
+        """
+        # inisiasi model yang diperlukan
         user_model = UserModel()
         order_model = OrderModel()
         user = user_model.get_current_user()
+
+        # cek jika user belum login maka redirect ke halaman login
+        if not session.get('username'):
+            flash('Kamu Belum Login!', 'error')
+            return redirect(url_for("AuthController:login_page"))
+
+        # mengambil data dari input user menjadi dictionary
         form = request.form.to_dict()
         form['user_id'] = user.get_id()
         form['order_time'] = datetime.now().strftime('%Y-%m-%d %X')
+
+        # cek jika berhasil melakukan order
         if order_model.insert(form):
             flash("Berhasil order event!", 'success')
         else:
